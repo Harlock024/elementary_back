1e9b588c2938a490880eac064e532fd7bce2ea9b
harlock024 <hadith024@gmail.com>
2025-11-18 23:32:49 -0600

    fix: int:id to uuid:int on url students

--- students/urls.py
+++ students/urls.py
@@ -7,5 +7,5 @@ from .views import StudentViewSet
 urlpatterns = [
     path('', StudentViewSet.as_view(), name='student-list'),
     path('',StudentViewSet.as_view(), name='  student-create'),
-    path('<int:pk>/', StudentViewSet.as_view(), name='student-detail'),
+    path('<uuid:pk>/', StudentViewSet.as_view(), name='student-detail'),
     ]
